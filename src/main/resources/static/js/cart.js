92862553f3976428be1c1c5f3427a1cabc4a1d56
daehan <dk8660@naver.com>
2025-02-13 11:49:34 +0900

    내 장바구니 페이지에 체크 박스와 총 금액 및 주문하기 버튼 추가

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -21,3 +21,50 @@ function addToCart(productId) {
         })
         .catch(error => console.error("Error: ", error));
 }
+
+function updateTotal() {
+    let total = 0;
+    let checkboxes = document.querySelectorAll('.product-checkbox');
+    checkboxes.forEach((checkbox) => {
+       if(checkbox.checked) {
+           let price = parseInt(checkbox.dataset.price);
+           let quantity = parseInt(checkbox.dataset.quantity);
+           total += price * quantity;
+       }
+    });
+    document.getElementById('total-price').innerText = total + '원';
+}
+
+function submitOrder() {
+    let selectedProducts = [];
+    let checkboxes = document.querySelectorAll('.product-checkbox');
+    checkboxes.forEach((checkbox) => {
+       if(checkbox.checked) {
+           selectedProducts.push({
+               productId: checkbox.value,
+               quantity: parseInt(checkbox.dataset.quantity, 10)
+           });
+       }
+    });
+
+    if(selectedProducts.length === 0) {
+        alert("주문할 상품을 선택해주세요.");
+        return;
+    }
+
+    let orderData = {
+        totalPrice: document.getElementById('total-price').innerText.replace("원", ""),
+        items: selectedProducts
+    }
+
+    let form = document.createElement("form");
+    form.method = "POST";
+    form.action = "/order/newOrder";
+    let input = document.createElement("input");
+    input.type = "hidden";
+    input.name = "orderData";
+    input.value = JSON.stringify(orderData);
+    form.appendChild(input);
+    document.body.appendChild(form);
+    form.submit();
+}
